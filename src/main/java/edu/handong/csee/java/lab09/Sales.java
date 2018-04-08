182e44ceca840ad308f14b40498747be8ce5e242
JeongSeop <wjdtjq1121@gmail.com>
2018-04-08 18:07:21 +0900

    add javadoc and single comments

--- src/main/java/edu/handong/csee/java/lab09/Sales.java
+++ src/main/java/edu/handong/csee/java/lab09/Sales.java
@@ -1,136 +1,123 @@
-package edu.handong.csee.java.lab09;
+package edu.handong.csee.java.lab09; // Set the package where this class belongs to
 
-import java.util.Scanner;
+import java.util.Scanner; // gets the Scanner class from the package (libarary) java.util
 
-public class Sales {
+/**
+ * This class defines to get the sales and names of salesmen. </br>
+ * gets total people from the user and get the highest sales person and average sales of salesmen.</br>
+ * @author HAN
+ */
+public class Sales { // its a class which can access from other package
 
-	private String name;
-	private double money;
+	private String name; // declares name member
+	private double money; // declares money member
 
-	private double highestSales;
-	private double averageSales;
-	private Sales[] team;
-	private int numTeam;
+	private double highestSales; // declares top sales member
+	private double averageSales; // declares aver. member
+	private Sales[] team; // declares the array Sales
+	private int numTeam; // declares total num of salesmen member
+
+	/**
+	 * This class defines to get the data sales and names of salesmen. </br>
+	 * gets total people from the user and store them in the array team.</br>
+	 * @author HAN
+	 */
+	public void getData() { // a method to get the data from the user 
+
+		Scanner myKeyboard = new Scanner(System.in); // sets up things so the program can accept keyboard input
+		System.out.println("Enter number of sales associates: "); // prints to ask the user to get the num of salesmen
+		this.numTeam = myKeyboard.nextInt(); // stores the value in numteam
 
 
-	public void getData() {
+		team = new Sales[numTeam]; // makes the number of user inputs with using array.
 
-		Scanner myKeyboard = new Scanner(System.in);
-		System.out.println("Enter number of sales associates: ");
-		this.numTeam = myKeyboard.nextInt();
+		for(int i=0; i<numTeam; i++) { // executes loop recurrence
+			team[i] = new Sales(); // instantiate array team.
+
+			System.out.println("Enter data for associate number " + (i+1)); // prints which num of men
+			System.out.println("Enter name of sales associate: "); // asks user to put the name of specific man
+			myKeyboard.nextLine(); // gets the enter key here, so error doesnt occur
+			team[i].name = myKeyboard.nextLine(); // stores the string value in the array team
+
+			System.out.println("Enter associate's sales: "); // asks user to put sales value
+			team[i].money = myKeyboard.nextDouble(); // stores the value in array team
+		}
+	}
 
 
-		team = new Sales[numTeam];
+	/**
+	 * This class defines to compute average and highest sales. </br>
+	 * @author HAN
+	 */
+	public void Cal() { // a method, to calculate the data
+		double sum=0; // initialize the sum member
+		double highestSales=0; // initialize the highestsales member
 
-		for(int i=0; i<numTeam; i++) {
-			team[i] = new Sales();
+		for(int i=0;i<numTeam;i++) { // executes for loop
 
-			System.out.println("Enter data for associate number " + (i+1));
-			System.out.println("Enter name of sales associate: ");
-			myKeyboard.nextLine();
-			team[i].name = myKeyboard.nextLine();
+			if(highestSales < team[i].money) // condition if the other guy earns more money
+				highestSales = team[i].money; // stores the value in highestsales member
 
-			System.out.println("Enter associate's sales: ");
-			team[i].money = myKeyboard.nextDouble();
-
+			sum+=team[i].money; // stack up the money value
 		}
 
+		averageSales = sum/numTeam; // divide by num of sales men to get average
 
+		this.highestSales = highestSales; // stores the value in the class, not in this local method member.
 	}
 
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
+	/**
+	 * This class shows the final result of average and highest sum and each salesman's name and sales. </br>
+	 * @author HAN
+	 */
+	public void print() { // a method to show the result of average, highest sales, and each salesman's name and sales.
 
 
-	public double getMoney() {
-		return money;
-	}
+		System.out.printf("Average sales per associate is $%.1f \n", this.averageSales); // prints the average sales
+		System.out.printf("The highest sales figure is $%.1f \n", this.highestSales); // prints the highest sales
 
+		System.out.println("The following had the highest sales: "); // prints what next step is coming
 
-	public void setMoney(double money) {
-		this.money = money;
-	}
+		for(int i=0; i<numTeam; i++) { // executes recurrence statement
+			if(team[i].money == this.highestSales) { // condition if the array index has the highest value
+				System.out.println("Name: " + this.team[i].name); // prints the name of array index
+				System.out.printf("Sales: %.1f \n", this.team[i].money); // prints the sales of array index
 
-
-	public void Cal() {
-
-
-		double sum=0;
-		double highestSales=0;
-
-		for(int i=0;i<numTeam;i++) {
-
-			if(highestSales < team[i].money)
-				highestSales = team[i].money;
-
-			sum+=team[i].money;
-		}
-
-		averageSales = sum/numTeam;
-
-		this.highestSales = highestSales;
-	}
-
-	public void print() {
-		
-		
-		System.out.printf("Average sales per associate is $%.1f \n", this.averageSales);
-		System.out.printf("The highest sales figure is $%.1f \n", this.highestSales);
-
-		System.out.println("The following had the highest sales: ");
-		
-		for(int i=0; i<numTeam; i++) {
-		if(team[i].money == this.highestSales) {
-			System.out.println("Name: " + this.team[i].name);
-			System.out.printf("Sales: %.1f \n", this.team[i].money);
-			
-			if(this.averageSales < this.team[i].money)			
-			System.out.printf("$%.1f above the average. \n", (this.team[i].money - this.averageSales));
-			else
-			System.out.printf("$%.1f below the average. \n", (this.averageSales - this.team[i].money));
+				if(this.averageSales < this.team[i].money)	// condition if the guy earns much money
+					System.out.printf("$%.1f above the average. \n", (this.team[i].money - this.averageSales)); // prints the guy earns much money.
+				else // otherwise
+					System.out.printf("$%.1f below the average. \n", (this.averageSales - this.team[i].money)); // prints the guy earns less money
 			}		
 		}
 
-		System.out.println("The rest performed as follows: ");
-		
-		for(int i=0; i<numTeam; i++) {
-			if(team[i].money != this.highestSales) {
-				System.out.println("Name: " + this.team[i].name);
-				System.out.printf("Sales: %.1f \n", this.team[i].money);
-				
-				if(this.averageSales < this.team[i].money)			
-				System.out.printf("$%.1f above the average. \n", (this.team[i].money - this.averageSales));
-				else
-				System.out.printf("$%.1f below the average. \n", (this.averageSales - this.team[i].money));
-				}		
-			}
-	
-	
-	
-	
-	
+		System.out.println("The rest performed as follows: "); // prints the other sales men info
+
+		for(int i=0; i<numTeam; i++) { // executes for loop
+			if(team[i].money != this.highestSales) { // condition the guys are not the highestsales 
+				System.out.println("Name: " + this.team[i].name); // prints the index of names
+				System.out.printf("Sales: %.1f \n", this.team[i].money); //prints the index of money
+
+				if(this.averageSales < this.team[i].money)			// condition they guy earns much money than average
+					System.out.printf("$%.1f above the average. \n", (this.team[i].money - this.averageSales)); // prints the sales the guy earns much money
+				else // otherwise
+					System.out.printf("$%.1f below the average. \n", (this.averageSales - this.team[i].money)); // prints the sales the guy earns less money
+			}		
+		}
+
 	}
 
+	/**
+	 * first, execute the instantiation. </br>
+	 * It calls three methods to get data and compute data and show the data.</br>
+	 * @author HAN
+	 */
+	public static void main(String[] args) { // a void method, invoked by the system
 
-	public static void main(String[] args) {
+		Sales company = new Sales();  // instantiate the sales names company
 
-		Sales company = new Sales(); 
-		
-		company.getData();
-		company.Cal();
-		company.print();
-		
-		
-		
-		
-
+		company.getData(); // calls a method getdata
+		company.Cal(); // calls a method to compute data
+		company.print(); // calls a method to print the all the statement of average, highest value, and each salesman's name and sales.
 	}
 
 }
